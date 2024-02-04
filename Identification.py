560a18085a227f66a01669ca99afbdab59315229
Om <omnagvekar29@gmail.com>
2024-02-05 00:33:39 +0530

    Added drop rate and frame option for faster processing

--- Identification.py
+++ Identification.py
@@ -40,10 +40,12 @@ class Detectron:
         return key
 
 class VideoProcessor(threading.Thread):
-    def __init__(self, video_path, detectron_instance):
+    def __init__(self, video_path, detectron_instance, desired_frame_rate, drop_rate):
         threading.Thread.__init__(self)
         self.video_path = video_path
         self.detectron_instance = detectron_instance
+        self.desired_frame_rate = desired_frame_rate
+        self.drop_rate = drop_rate
 
     def run(self):
         cap = cv2.VideoCapture(self.video_path)
@@ -51,15 +53,35 @@ class VideoProcessor(threading.Thread):
             print("Error in Video")
             return
 
+        frame_count = 0
         while True:
+            start_time = time.time()  # Start time for frame processing
             success, image = cap.read()
             if not success:
                 break
 
-            self.detectron_instance.predict_on_image(image)
-            key = self.detectron_instance.show_result()
+            # Drop frames based on the drop rate
+            if frame_count % (self.drop_rate + 1) == 0:
+                # Perform detection on the current frame
+                self.detectron_instance.predict_on_image(image)
+                key = self.detectron_instance.show_result()
+
+            # Calculate the time taken for frame processing
+            end_time = time.time()
+            frame_processing_time = end_time - start_time
+
+            # Sleep to achieve desired frame rate (e.g., 30 FPS)
+            time_to_sleep = max(0, 1 / self.desired_frame_rate - frame_processing_time)
+            time.sleep(time_to_sleep)
+
+            # Increment frame count
+            frame_count += 1
+
+            # Check if the window is closed
             if cv2.getWindowProperty("Result", cv2.WND_PROP_VISIBLE) < 1:
                 break
+
+
 
 if __name__ == "__main__":
     setup_logger()
@@ -67,5 +89,5 @@ if __name__ == "__main__":
     Object.configure()
     Object.get_predictor()
     video_path = "video/video1.mp4"
-    video_processor = VideoProcessor(video_path, Object)  # Pass Object instance
+    video_processor = VideoProcessor(video_path, Object,30, 10)  # Pass Object instance
     video_processor.start()
